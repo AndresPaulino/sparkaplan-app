84ad41426b2c040d5b931126ae374c866c0bea15
Andres Paulino <paulinoandresmiguel@gmail.com>
2023-05-26 19:34:30 -0400

    Initial commit - changed hero section

---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+// next
+import Head from 'next/head';
+// @mui
+import { Box } from '@mui/material';
+// layouts
+import MainLayout from '../layouts/main';
+// components
+import ScrollProgress from '../components/scroll-progress';
+// sections
+import {
+  HomeHero,
+  HomeMinimal,
+  HomeDarkMode,
+  HomeLookingFor,
+  HomeForDesigner,
+  HomeColorPresets,
+  HomePricingPlans,
+  HomeAdvertisement,
+  HomeCleanInterfaces,
+  HomeHugePackElements,
+} from '../sections/home';
+
+// ----------------------------------------------------------------------
+
+HomePage.getLayout = (page) => <MainLayout> {page} </MainLayout>;
+
+// ----------------------------------------------------------------------
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title> Sparkaplan - The AI Lesson Plan Generator for Teachers</title>
+      </Head>
+
+      <ScrollProgress />
+
+      <HomeHero />
+
+      <Box
+        sx={{
+          overflow: 'hidden',
+          position: 'relative',
+          bgcolor: 'background.default',
+        }}
+      >
+        <HomeMinimal />
+
+        <HomeHugePackElements />
+
+        <HomeForDesigner />
+
+        <HomeDarkMode />
+
+        <HomeColorPresets />
+
+        <HomeCleanInterfaces />
+
+        <HomePricingPlans />
+
+        <HomeLookingFor />
+
+        <HomeAdvertisement />
+      </Box>
+    </>
+  );
+}
